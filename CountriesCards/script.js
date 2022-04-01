2e15b898b8fcaf99e24a7e8912a574fc6bfd80f1
dilmurodov <tolibdilmurodov98@gmail.com>
2022-04-01 14:37:58 +0500

    [init] upload project image

--- CountriesCards/script.js
+++ CountriesCards/script.js
@@ -4,3 +4,28 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.com/v2/all');
+request.send();
+
+request.addEventListener('load', function () {
+    let data = JSON.parse(request.responseText);
+    console.log(data);
+    data.forEach(data => {
+        let template = `
+    <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(data.population/1000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages ? data.languages[0].name : data.languages}</p>
+            <p class="country__row"><span>💰</span>${data.currencies ? data.currencies[0].name : data.currencies}</p>
+          </div>
+        </article>
+    `
+    countriesContainer.insertAdjacentHTML('afterend', template);
+    countriesContainer.getElementsByClassName.opacity = 1;
+    })  
+})
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 1. Project flipCards
 
 ![Project image](./flipSectionParts/media/project-img.png)
+
+2. Project Countries Card
+
+![Project image](./CountriesCards/complated_image.png)
+
+### Used:
+* AJAX/JS
+* HTML/CSS
+
+3. Project Breakout Game
+
+![Project image](./break_game/complated_image.png)
+
+* CANVAS/JS
