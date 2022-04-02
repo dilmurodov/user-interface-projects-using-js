a5be6bd0ad96862ff8215c8220a21bd521b8ac14
dilmurodov <tolibdilmurodov98@gmail.com>
2022-04-02 11:14:17 +0500

    [feat] update project

--- CountriesCards/script.js
+++ CountriesCards/script.js
@@ -64,8 +64,8 @@ class Card {
             <h3 class="country__name">${data.name}</h3>
             <h4 class="country__region">${data.region}</h4>
             <p class="country__row"><span>👫</span>${(
-              data.population / 1000
-            ).toFixed(1)} people</p>
+              new Intl.NumberFormat('uz-UZ').format(data.population)
+            )}  people</p>
             <p class="country__row"><span>🗣️</span>${
               data.languages ? data.languages[0].name : data.languages
             }</p>
